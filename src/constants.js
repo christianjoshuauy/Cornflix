6b4525043e2f478f4a61b0b1db6a642169dfde74
tr1b1iwnl <tr1b1iwnl@gmail.com>
2023-06-06 20:05:57 +0800

    Removed one month ago fetch use

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,5 @@
-import { getOneMonthAgoReleaseDate } from "./utils";
-
 export const LANG = "en-US";
 export const REGION = "US";
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
-const ONEMONTHAGO = getOneMonthAgoReleaseDate();
 const { REACT_APP_TMDB_API_KEY } = process.env;
 export const fetchSearchQuery = `search/multi?api_key=${REACT_APP_TMDB_API_KEY}&language=${LANG}&query=`;
